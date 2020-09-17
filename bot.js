fe921f2034352b580cf0e32540d4ce8a7ccb4a3e
MowuHacks <71355157+MowuHacks@users.noreply.github.com>
2020-09-17 13:02:37 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const { prefix } = require('./config.json');
 const client = new Discord.Client();
 
 client.on('ready', () => {
@@ -6,9 +7,15 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+	if (message.content === `${prefix}ping`) {
+		message.channel.send('Pong.');
+	} else if (message.content === `${prefix}beep`) {
+		message.channel.send('Boop.');
+	} else if (message.content === `${prefix}server`) {
+		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+	} else if (message.content === `${prefix}user-info`) {
+		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+	}
 });
 
 client.on('guildMemberAdd', member => {
